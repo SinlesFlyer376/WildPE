c6c9a7cabcb16c34e334ce05c8f630e47a2a0ca3
Shane Malin <shanemalin23@gmail.com>
2017-03-04 23:54:18 -0500

    Update wild.php

--- src/WildPE/wild.php
+++ src/WildPE/wild.php
@@ -1,17 +1,26 @@
 <?php namespace WildPE;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\command\{Command,CommandSender};
+
+use pocketmine\command\{
+	Command,
+	CommandSender
+};
+
 use pocketmine\math\Vector3;
-use pocketmine\Player;
 use pocketmine\utils\TextFormat as TF;
-use pocketmine\Player\NoFallDamage;
+use pocketmine\Player;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 
 class wild extends PluginBase implements  Listener {
 	
+	public $immune = [];
+	
 	public function onEnable(){
-		$this->getLogger()->info(TF::AQUA . "Enabled Plugin WildPE by SinlesFlyer");
+		$this->getLogger()->info(TF::AQUA . "[WildPE] Enabled!");
 	}
 
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
@@ -19,8 +28,9 @@ class wild extends PluginBase implements  Listener {
 			case "wild":
 				if($sender instanceof Player) {
 					$x = mt_rand(-75000,75000);
-            				$y = rand(70,90);
+            				$y = 128; //Todo: safe spawning
 					$z = mt_rand(-75000,75000);
+					$this->immune[$p->getName()] = time();
 					$sender->teleport($sender->getLevel()->getSafeSpawn(new Vector3($x, $y, $z)));
 					$sender->sendTip(TF::AQUA . "[Wild]" . TF::GRAY . " You've been teleported somewhere random!");
 					$sender->sendMessage(TF::AQUA . "[Wild]" . TF::GRAY . " Teleporting to: X-" . $x . " Z-" . $z);
@@ -29,5 +39,21 @@ class wild extends PluginBase implements  Listener {
 				}
 			break;
 		}
+	}
+	
+	public function onDmg(EntityDamageEvent $e) {
+		$p = $e->getEntity();
+		$cause = $e->getCause();
+		if($p instanceof Player) {
+			if($cause == EntityDamageEvent::CAUSE_FALL && isset($this->immune[$p->getName()])) {
+				$e->setCancelled(true); //Cancels fall damage after wild teleporting
+				unset($this->immune[$p->getName()]);
+				return;
+			}
+		}
+	}
+	
+	public function onQuit(PlayerQuitEvent $e){
+		if(isset($this->immune[$p->getName()])) unset($this->immune[$p->getName()]);
 	}
 }
